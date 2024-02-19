cc1e3bae7f6f5d0faf1e863c55a0b6194cd9fc7f
Bobishajohn <bobishajohn@gmail.com>
2024-02-19 12:20:04 +0530

    first

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+st.title('FIRST APP')
+username = st.text_input('Username')
+password = st.text_input('Password',type='password')
+if st.button('Login'):
+    if username == 'babisha' and password=='password':
+        #st.write('Login Granted') 
+        st.warning('Login Granted')    
+    else:
+       # st.write('Acces denied') 
+        st.warning('Acces denied')    
